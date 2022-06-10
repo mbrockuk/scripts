218b82a463fae57096c0e48585b2238d5afd463a
Matt Brocklehurst <brockuk@protonmail.com>
2022-06-10 18:56:59 +0100

    Added documents folder

--- backup-lenovo.sh
+++ backup-lenovo.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-borg create /mnt/sdcard/backup::archive-{hostname}-{now}  ~/dev/
+borg create /mnt/sdcard/backup::archive-{hostname}-{now}  \
+	~/dev/ \
+	~/Documents/ 
+
 borg prune -v --list --keep-within=3d /mnt/sdcard/backup
 rclone sync /mnt/sdcard/backup backblaze:/borgrepo-lenovo/
